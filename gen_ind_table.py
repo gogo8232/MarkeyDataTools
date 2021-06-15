7243202b45e08090d2eb9d31c011f63b6f1fda98
Lee Park <lee.park@uky.edu>
2021-06-15 15:40:43 -0400

    Add files via upload

--- gen_ind_table.py
+++ gen_ind_table.py
@@ -30,7 +30,25 @@ class gen_ind_table:
         total.columns = ['County','total']
         self.total = total
         df.drop(table+'_001E', axis = 1, inplace = True)
+        if table == 'B01001':
+            df.drop([table+'_002E', table+'_026E'], axis = 1, inplace = True)    
+        elif bool(re.match(re.compile('B01001.+'), table)):
+            df.drop([table+'_002E', table+'_017E'], axis = 1, inplace = True)       
+        first_four = df.columns.to_list()[:4]
+        last_col = df.columns.to_list()[4:]
+        last_col.sort()
+        col = first_four + last_col
+        df.columns = col
         rename_dict = dict(zip(df.columns[df.columns.str.match(re.compile(f'{table}.*'))].to_series().sort_values().reset_index(drop = True), var_desc.iloc[:, -1]))
+        if bool(re.match(re.compile('B01001.*'), table)):
+            col = var_desc.columns.to_list()[:3]
+            col.append('sex')
+            col.append('age')
+            var_desc.columns = col
+            pattern = re.compile(f'{table}_.*')
+            rename_dict = dict(zip(df.columns[df.columns.str.match(re.compile(f'{table}.*'))].to_series().sort_values().reset_index(drop = True), 
+                                   var_desc[var_desc.name.str.match(pattern)].iloc[:, -2:].apply(lambda x: x.sex + "_" + x.age, axis = 1)))
+            
         df.rename(columns = rename_dict, inplace = True)
         df['total'] = total.total
         self.df = df
